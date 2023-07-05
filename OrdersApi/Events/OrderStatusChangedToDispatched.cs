28be3c4340ef823706b7628ba70ca12013d00586
Usman Tahir <Usman.tahir@synergy-it.com>
2023-07-06 00:08:59 +0500

    Adding the OrderDispatchedEvent Consumer part of the OrdersApi

---
+++ OrdersApi/Events/OrderStatusChangedToDispatched.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OrdersApi.Events
+{
+    public class OrderStatusChangedToDispatched
+    {
+        public Guid OrderId { get; set; }
+        public DateTime DispatchDateTime { get; set; }
+
+    }
+}
